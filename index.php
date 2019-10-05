784864cbac91dbc0522f10f827ebd8074e6e70c2
zohrabahundov <ahundovzs@gmail.com>
2019-10-05 12:54:23 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>ubutia | Home</title>
+        <title>ubutia | Home - <?php echo date( "d.m.y H:i" );?></title>
         <link rel="shortcut icon" type="image/x-icon" href="assets/images/logo-icon.png"/>
 
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
